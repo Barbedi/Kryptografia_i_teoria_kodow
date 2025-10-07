27c0250bc6f477a24148270393f20fab248316f7
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-10-07 16:03:25 +0200

    Initialize Next.js and Electron project setup

---
+++ src/app/main/main.tsx
@@ -0,0 +1,10 @@
+
+
+const main = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+            <h1>Kryptografia i teoria kodów-projekt</h1>
+    </div>
+  );
+};
+export default main;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-center">
+      <h1>to jest Test</h1>
+      <a href="/src/app/main/main.tsx">Przejdź do Main</a>
+    </div>
+  );
+}
